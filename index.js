7e9c9d22ff9afa8ec85008782d11c1aff625be7a
Ryley Watson <ryleyswatson@gmail.com>
2025-01-23 11:03:57 -0800

    Completes Lab but it was dumb and I had to do it without map function due to random letter capitalization

--- index.js
+++ index.js
@@ -12,5 +12,16 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return [
+    "What Does The This Keyword Mean?",
+    "What Is The Constructor OO Pattern?",
+    "Implementing Blockchain Web API",
+    "The Test Driven Development Workflow",
+    "What Is NaN And How Can We Check For It",
+    "What Is The Difference Between StopPropagation And PreventDefault?",
+    "Immutable State And Pure Functions",
+    "What Is The Difference Between == And ===?",
+    "What Is The Difference Between Event Capturing And Bubbling?",
+    "What Is JSONP?"
+  ];
+};
